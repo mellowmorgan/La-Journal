469c08cfb299b6440eb850cbb734549e56d6c87d
mellowmorgan <morgan.waites@gmail.com>
2021-12-11 22:48:26 -0800

    static stuff wip

--- django_journal_project/settings.py
+++ django_journal_project/settings.py
@@ -127,7 +127,7 @@ STATICFILES_DIRS = [
 ]
 #STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-# STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 CRISPY_TEMPLATE_PACK='bootstrap4'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'

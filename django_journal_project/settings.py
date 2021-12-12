f66f931eb3c90f1e70efeffaeeb624ba053d6e66
mellowmorgan <morgan.waites@gmail.com>
2021-12-11 23:31:15 -0800

    wip static

--- django_journal_project/settings.py
+++ django_journal_project/settings.py
@@ -126,7 +126,6 @@ USE_TZ = True
 #    "blog/static",
 # ]
 STATIC_URL = '/static/'
-STATICFILES_DIRS= (os.path.join(BASE_DIR, 'staticfiles'),)
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 

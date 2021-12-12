0c0cc3a92ace3385cddec2042284e7b7921c90e6
mellowmorgan <morgan.waites@gmail.com>
2021-12-11 22:53:10 -0800

    static stuff wip

--- django_journal_project/settings.py
+++ django_journal_project/settings.py
@@ -121,10 +121,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATICFILES_DIRS = [
+# STATICFILES_DIRS = [
    
-   "blog/static",
-]
+#    "blog/static",
+# ]
+STATICFILES_DIRS= (os.path.join(BASE_DIR, 'static'),)
 #STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = '/static/'
